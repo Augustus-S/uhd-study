47ecf68861285b65adcd07220cca47e5d866c9b4
shiwenhao <swh18956292867@outlook.com>
2025-06-24 20:03:55 +0800

    为rx_samples_to_file.cpp进行少量修改以适应现代C++

--- host/examples/rx_samples_to_file.cpp
+++ host/examples/rx_samples_to_file.cpp
@@ -125,7 +125,7 @@ double disk_rate_check(const size_t sample_type_size,
     );
     std::regex_match(dd_output, dd_matchs, dd_regex);
 
-    if ((dd_output.length() == 0) || (dd_matchs[0].str() != dd_output)) {
+    if ((dd_output.empty()) || (dd_matchs[0].str() != dd_output)) {
         std::cerr << err_msg << std::endl;
     } else {
         double disk_rate_sigfigs = std::stod(dd_matchs[1]);
@@ -153,7 +153,7 @@ double disk_rate_check(const size_t sample_type_size,
 
 
 template <typename samp_type>
-void recv_to_file(uhd::usrp::multi_usrp::sptr usrp,
+void recv_to_file(const uhd::usrp::multi_usrp::sptr& usrp,  // 原代码：uhd::usrp::multi_usrp::sptr usrp,
     const std::string& cpu_format,
     const std::string& wire_format,
     const std::vector<size_t>& channel_nums,
@@ -215,8 +215,11 @@ void recv_to_file(uhd::usrp::multi_usrp::sptr usrp,
                 if (file.find('.') != std::string::npos) {
                     const std::string base_name = file.substr(0, file.find_last_of('.'));
                     const std::string extension = file.substr(file.find_last_of('.'));
-                    filename = base_name + "_" + "ch" + std::to_string(channel_nums[ch])
-                               + extension;
+                    filename = std::string(base_name)
+                                   .append("_")
+                                   .append("ch")
+                                   .append(std::to_string(channel_nums[ch]))
+                                   .append(extension);
                 } else {
                     // file extension does not exist
                     filename = file + "_" + "ch" + std::to_string(channel_nums[ch]);
@@ -322,9 +325,9 @@ void recv_to_file(uhd::usrp::multi_usrp::sptr usrp,
     stream_cmd.stream_mode = uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS;
     rx_stream->issue_stream_cmd(stream_cmd);
 
-    for (size_t i = 0; i < outfiles.size(); i++) {
-        if (outfiles[i].is_open()) {
-            outfiles[i].close();
+    for (auto & outfile : outfiles) {
+        if (outfile.is_open()) {
+            outfile.close();
         }
     }
 
@@ -345,8 +348,8 @@ void recv_to_file(uhd::usrp::multi_usrp::sptr usrp,
         if (enable_size_map) {
             std::cout << std::endl;
             std::cout << "Packet size map (bytes: count)" << std::endl;
-            for (SizeMap::iterator it = mapSizes.begin(); it != mapSizes.end(); it++)
-                std::cout << it->first << ":\t" << it->second << std::endl;
+            for (auto & mapSize : mapSizes)
+                std::cout << mapSize.first << ":\t" << mapSize.second << std::endl;
         }
     }
 }
@@ -414,9 +417,9 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
         ("args", po::value<std::string>(&args)->default_value(""), "multi uhd device address args")
         ("file", po::value<std::string>(&file)->default_value("usrp_samples.dat"), "name of the file to write binary samples to")
         ("type", po::value<std::string>(&type)->default_value("short"), "sample type: double, float, or short")
-        ("nsamps", po::value<size_t>(&total_num_samps)->default_value(0), "total number of samples to receive")
+        ("nsamps", po::value<size_t>(&total_num_samps)->default_value(400000), "total number of samples to receive")
         ("duration", po::value<double>(&total_time)->default_value(0), "total number of seconds to receive")
-        ("spb", po::value<size_t>(&spb)->default_value(10000), "samples per buffer")
+        ("spb", po::value<size_t>(&spb)->default_value(1000), "samples per buffer")
         ("rate", po::value<double>(&rate)->default_value(10e6), "rate of incoming samples")
         ("freq", po::value<double>(&freq)->default_value(2407e6), "RF center frequency in Hz")
         ("lo-offset", po::value<double>(&lo_offset)->default_value(0.0),
@@ -424,7 +427,7 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
         ("gain", po::value<double>(&gain), "gain for the RF chain")
         ("ant", po::value<std::string>(&ant), "antenna selection")
         ("subdev", po::value<std::string>(&subdev), "subdevice specification")
-        ("channels,channel", po::value<std::string>(&channels)->default_value("0"), "which channel(s) to use (specify \"0\", \"1\", \"0,1\", etc)")
+        ("channels,channel", po::value<std::string>(&channels)->default_value("0"), R"(which channel(s) to use (specify "0", "1", "0,1", etc))")    // 使用 Raw String Literal 语法
         ("bw", po::value<double>(&bw), "analog frontend filter bandwidth in Hz")
         ("ref", po::value<std::string>(&ref), "reference source (internal, external, mimo)")
         ("wirefmt", po::value<std::string>(&wirefmt)->default_value("sc16"), "wire format (sc8, sc16 or s16)")  // sc is "short complex"
@@ -472,9 +475,9 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
 
     // Parse channel selection string
     boost::split(channel_strings, channels, boost::is_any_of("\"',"));
-    for (size_t ch = 0; ch < channel_strings.size(); ch++) {
+    for (const auto& ch : channel_strings) {
         try {
-            int chan = std::stoi(channel_strings[ch]);
+            int chan = std::stoi(ch);
             if (chan >= static_cast<int>(usrp->get_rx_num_channels()) || chan < 0) {
                 throw std::runtime_error("Invalid channel(s) specified.");
             } else {
@@ -638,7 +641,7 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
         th_prefix)
 
     for (size_t i = 0; i < channel_list.size(); i++) {
-        std::string th_prefix = "";
+        std::string th_prefix;
         if (multithread) {
             chans_in_thread.clear();
             chans_in_thread.push_back(channel_list[i]);
@@ -654,7 +657,7 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
          *
          * !! recv_to_file_args中第九个参数为 rates， 对应了 recv_to_file 的第九个参数 bw.
          */
-        threads.push_back(std::thread([=, &rates]() {
+        threads.emplace_back([=, &rates]() {    // threads.push_back(std::thread([=, &rates]() -> threads.emplace_back([=, &rates]()
             // recv to file
             if (wirefmt == "s16") {
                 if (type == "double")
@@ -675,7 +678,7 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
                 else
                     throw std::runtime_error("Unknown type " + type);
             }
-        }));
+        });
         if (!multithread) {
             break;
         }
@@ -693,7 +696,7 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
 
     const auto end_time = std::chrono::steady_clock::now() + (total_time - 1) * 1s;
 
-    while (threads.size() > 0   // 还有线程没运行完
+    while (!threads.empty()   // 还有线程没运行完
            && (std::chrono::steady_clock::now() < end_time || total_time == 0)  // 判断是否到了限定时间
            && !stop_signal_called) {    // 信号停止中断是否被触发
         std::this_thread::sleep_for(1s);
@@ -711,7 +714,7 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
             }
         }
         // Report the bandwidth of remaining threads
-        if (bw_summary && threads.size() > 0) {
+        if (bw_summary && !threads.empty()) {
             const std::lock_guard<std::mutex> lock(recv_mutex);
             std::cout << "\t"
                       << (std::accumulate(std::begin(rates), std::end(rates), 0) / 1e6
@@ -721,9 +724,9 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
     }
 
     // join any remaining threads
-    for (size_t i = 0; i < threads.size(); i++) {
-        if (threads[i].joinable()) {
-            threads[i].join();
+    for (auto & thread : threads) {
+        if (thread.joinable()) {
+            thread.join();
         }
     }
 
